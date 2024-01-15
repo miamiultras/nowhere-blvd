29e44842d4d366938f7c67dc75435493278a8442
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2024-01-15 19:30:55 +0100

    add log to auth

--- auth.ts
+++ auth.ts
@@ -33,7 +33,8 @@ export const { auth, signIn, signOut } = NextAuth({
  
           if (passwordsMatch) return user;
         }
-
+        
+        console.log('Invalid credentials');
         return null;
       },
     }),
